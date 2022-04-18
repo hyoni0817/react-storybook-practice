be2cc68438965b0cededa55a3d70ffc8e196ae17
hyoni0817 <hyoni0817@naver.com>
2022-04-18 22:05:07 +0900

    Set: extends에 prettier 추가 및 "에서 '로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {},
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 };
