8a71dc59cc3d8fbd0581571c1de28a2ca7351cb8
hyoni0817 <hyoni0817@naver.com>
2022-04-23 23:39:11 +0900

    Set: testing-library와 jest를 사용한 테스트 환경 구축

---
+++ setupTests.js
@@ -0,0 +1,8 @@
+// setupTests.js
+
+import { setGlobalConfig } from '@storybook/testing-react';
+
+// Storybook's preview file location
+import * as globalStorybookConfig from './.storybook/preview';
+
+setGlobalConfig(globalStorybookConfig);
